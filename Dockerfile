b861d2dafad50977b1f2c872e5657227fc2ad4a5
Pratik Jagrut <pjagrut@redhat.com>
2019-05-30 20:12:40 +0530

    Improvising api for fetching latest commit id of the repo. Using golang git pkg. Adding ca-certificate to the apline image.
    Signed-off-by: Pratik Jagrut <pjagrut@redhat.com>

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
 # build stage
 FROM golang:alpine AS build-env
 ADD . /src
+RUN apk add -q git
+RUN go get gopkg.in/src-d/go-git.v4 
 RUN cd /src && go build -o app
 
 # final stage
 FROM alpine
+RUN apk update && apk add ca-certificates && rm -rf /var/cache/apk/*
 WORKDIR /app
 COPY --from=build-env /src/app /app/
 ENTRYPOINT ./app
--- main.go
+++ main.go
@@ -17,7 +17,7 @@ type Status struct {
 type Current []Status
 
 func handleRequests() {
-	http.HandleFunc("/api/status", returnStatus)
+	http.HandleFunc("/", returnStatus)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -29,18 +29,29 @@ func returnStatus(w http.ResponseWriter, r *http.Request) {
 }
 
 func getCommitID() string {
-	_ = os.RemoveAll("/tmp/foo")
-	repo, err := git.PlainClone("/tmp/foo", false, &git.CloneOptions{
+
+	//store log in file
+	f, err := os.OpenFile("text.log",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+
+	logger := log.New(f, "prefix", log.LstdFlags)
+	dir := "/tmp/foo"
+	err = os.RemoveAll(dir)
+	logger.Println(err)
+
+	repo, err := git.PlainClone(dir, false, &git.CloneOptions{
 		URL:      "https://github.com/pratikjagrut/status-rest-api.git",
 		Progress: os.Stdout,
 	})
-	if err != nil {
-		log.Fatal(err)
-	}
+	logger.Println(err)
+
 	head, err := repo.Head()
-	if err != nil {
-		log.Fatal(err)
-	}
+	logger.Println(err)
+
 	arr := head.Strings()
 	return arr[1]
 }
